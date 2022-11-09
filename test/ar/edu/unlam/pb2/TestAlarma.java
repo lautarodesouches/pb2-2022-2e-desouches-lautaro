2ed09884dce626f70206468fe5137a709dac20cf
UNLMLAB\laboratorios <mmartinez528@alumno.unlam.edu.ar>
2022-11-09 08:43:14 -0300

    Test

---
+++ test/ar/edu/unlam/pb2/TestAlarma.java
@@ -0,0 +1,19 @@
+package ar.edu.unlam.pb2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAlarma {
+
+	@Test
+	public void queSePuedaRegistrarUnaAlarmaEnLaCentral() {
+		
+	}
+	
+	@Test
+	public void queSePüedaAgregarUnUsuarioConfiguradorAUnaAlarma() {
+		
+	}
+
+}
